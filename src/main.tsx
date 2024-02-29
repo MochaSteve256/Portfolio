4b87bca7c0b37ace0a4888150d4108887e797073
Adrian <adrian.steyer@posteo.de>
2024-02-29 15:29:39 +0100

    html improvements

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ camera.rotation.x = cameraRotationOffsetX;
 camera.rotation.y = cameraRotationOffsetY;
 camera.rotation.z = cameraRotationOffsetZ;
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg") as HTMLCanvasElement
+  canvas: document.querySelector("#bg") as HTMLCanvasElement,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
@@ -44,7 +44,7 @@ scene.background = texture;
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.04);
 scene.add(ambientLight);
 const pointLight = new THREE.DirectionalLight(0xffffff, 10);
-pointLight.position.set(.8, .3, 4.8);
+pointLight.position.set(0.8, 0.3, 4.8);
 scene.add(pointLight);
 
 // helpers
@@ -59,62 +59,56 @@ scene.add(cube);
 
 // github skyline mesh
 const skylineLoader = new STLLoader();
-skylineLoader.load(
-  "MochaSteve256-2023.stl",
-  function (geometry) {
-    const material = new THREE.MeshPhongMaterial({
-      color: 0x005533,
-      specular: 0x111111,
-      shininess: 200,
-      flatShading: true
-    });
-    const mesh = new THREE.Mesh(geometry as THREE.BufferGeometry, material);
-    mesh.scale.set(0.02, 0.02, 0.02);
-    setRotation(mesh, -0.5 * Math.PI, 0, 0);
-    mesh.position.x = -2;
-    mesh.position.z = -4;
-    scene.add(mesh);
-  }
-)
+skylineLoader.load("MochaSteve256-2023.stl", function (geometry) {
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x005533,
+    specular: 0x111111,
+    shininess: 200,
+    flatShading: true,
+  });
+  const mesh = new THREE.Mesh(geometry as THREE.BufferGeometry, material);
+  mesh.scale.set(0.04, 0.04, 0.04);
+  setRotation(mesh, -0.5 * Math.PI, 0, -0.5 * Math.PI);
+  mesh.position.x = 2;
+  mesh.position.z = -2;
+  scene.add(mesh);
+});
 
 // generate stars
-function addStar(hRange: number, vRange: number, hOffset: number, vOffset: number) {
+function addStar(
+  hRange: number,
+  vRange: number,
+  hOffset: number,
+  vOffset: number,
+) {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   // material colors
   const colors = [
-    0xffffff,
-    0xffffff,
-    0xffffff,
-    0xffffff,
-    0xffffff,
-    0xffffff,
-    0xffffff,
-    0xffffff,
-    0x7700ff,
-    0x00ff00,
-    0xffaa00,
-    0xff0000,
-    0x00ffff,
-    0xffff00
-  ]
+    0xffffff, 0xffffff, 0xffffff, 0xffffff, 0xffffff, 0xffffff, 0xffffff,
+    0xffffff, 0x7700ff, 0x00ff00, 0xffaa00, 0xff0000, 0x00ffff, 0xffff00,
+  ];
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   const material = new THREE.MeshBasicMaterial({ color: randomColor });
   const star = new THREE.Mesh(geometry, material);
-  
+
   let [x, y, z] = Array(3).fill(0);
   x = Math.random() * hRange - hOffset;
   y = Math.random() * vRange - vOffset;
   z = Math.random() * hRange - hOffset;
-  
+
   star.position.set(x, y, z);
   scene.add(star);
 }
-Array(1500).fill(0).forEach(() => {
-  addStar(300, 320, 150, 20);
-});
-Array(100).fill(0).forEach(() => {
-  addStar(80, 80, 40, 40);
-})
+Array(1500)
+  .fill(0)
+  .forEach(() => {
+    addStar(300, 320, 150, 20);
+  });
+Array(100)
+  .fill(0)
+  .forEach(() => {
+    addStar(80, 80, 40, 40);
+  });
 
 // window resize
 function onWindowResize() {
@@ -122,46 +116,67 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
 
-function setRotation(obj: THREE.Object3D, x: number, y: number, z: number): void {
-  const qX = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), x);
-  const qY = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), y);
-  const qZ = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), z);
-  
+function setRotation(
+  obj: THREE.Object3D,
+  x: number,
+  y: number,
+  z: number,
+): void {
+  const qX = new THREE.Quaternion().setFromAxisAngle(
+    new THREE.Vector3(1, 0, 0),
+    x,
+  );
+  const qY = new THREE.Quaternion().setFromAxisAngle(
+    new THREE.Vector3(0, 1, 0),
+    y,
+  );
+  const qZ = new THREE.Quaternion().setFromAxisAngle(
+    new THREE.Vector3(0, 0, 1),
+    z,
+  );
+
   //reset rotation
   obj.quaternion.set(0, 0, 0, 1);
-  
+
   obj.quaternion.multiply(qX);
   obj.quaternion.multiply(qY);
   obj.quaternion.multiply(qZ);
 }
-function lerpVectors(vec1: THREE.Vector3, vec2: THREE.Vector3, amount: number): THREE.Vector3 {
+function lerpVectors(
+  vec1: THREE.Vector3,
+  vec2: THREE.Vector3,
+  amount: number,
+): THREE.Vector3 {
   const x = vec1.x * amount + vec2.x * (1 - amount);
   const y = vec1.y * amount + vec2.y * (1 - amount);
   const z = vec1.z * amount + vec2.z * (1 - amount);
   return new THREE.Vector3(x, y, z);
 }
-function rotateObjectToPoint(object: THREE.Object3D, targetPoint: THREE.Vector3): void {
+function rotateObjectToPoint(
+  object: THREE.Object3D,
+  targetPoint: THREE.Vector3,
+): void {
   // Calculate the direction vector from the object's position to the target point
   const direction = new THREE.Vector3();
   direction.subVectors(targetPoint, object.position).normalize();
-  
+
   // Create a lookAt matrix to orient the object towards the target point
   const lookAtMatrix = new THREE.Matrix4();
   lookAtMatrix.lookAt(object.position, targetPoint, object.up);
-  
+
   // Extract the rotation quaternion from the lookAt matrix
   const quaternion = new THREE.Quaternion();
   quaternion.setFromRotationMatrix(lookAtMatrix);
-  
+
   // Apply the rotation to the object
   object.quaternion.copy(quaternion);
 }
 // move stuff on scroll
 const smoothDeltaThreshold: number = 200;
 const smoothVelocityThreshold: number = 20;
-const acceleration: number = .8;
+const acceleration: number = 0.8;
 let smoothT: number = 0;
 let tV: number = 0;
 function moveCamera(): void {
@@ -169,21 +184,16 @@ function moveCamera(): void {
   if (delta < 0 && Math.abs(delta) > smoothDeltaThreshold) {
     tV += acceleration;
     if (tV < 0)
-      if (Math.abs(tV) > smoothVelocityThreshold)
-      tV -= acceleration;
-      else
-        tV += acceleration / 5;
-  }
-  else if (delta > 0 && Math.abs(delta) > smoothDeltaThreshold) {
+      if (Math.abs(tV) > smoothVelocityThreshold) tV -= acceleration;
+      else tV += acceleration / 5;
+  } else if (delta > 0 && Math.abs(delta) > smoothDeltaThreshold) {
     tV -= acceleration;
     if (tV > 0)
-      if (Math.abs(tV) > smoothVelocityThreshold)
-        tV += acceleration;
-      else
-        tV -= acceleration / 5;
+      if (Math.abs(tV) > smoothVelocityThreshold) tV += acceleration;
+      else tV -= acceleration / 5;
   }
   smoothT += tV;
-  tV *= .97;
+  tV *= 0.97;
 
   console.log(smoothT, tV);
 
